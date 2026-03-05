94d9049a9ee112b7590b0d260c05cbbb0e9dc17a
Tô Minh Nghĩa <minhnghia2010200500@gmail.com>
2026-03-05 23:28:35 +0700

    Add CORS config

---
+++ backend/src/main/java/com/vtmen/backend/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.vtmen.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // This allows any website to connect (great for testing)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
